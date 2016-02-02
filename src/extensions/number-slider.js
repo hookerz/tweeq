d770f55b04ceb5b72c61ae203a1ac19772dea500
Chris Parker <parker@byhook.com>
2016-02-02 17:20:20 -0500

    make the number slider interactive

--- src/extensions/number-slider.js
+++ src/extensions/number-slider.js
@@ -10,16 +10,29 @@ function fit(value, options) {
 
 function render(control, el) {
 
-  let { name, value } = control;
+  let { name, value, options } = control;
 
-  let onChange = event => control.update(event.target.value);
+  let onClick = event => {
+
+    let relative = event.offsetX / event.currentTarget.clientWidth;
+    let absolute = options.min + (options.max - options.min) * relative;
+
+    control.update(absolute);
+
+  }
+
+  let onMouseMove = event => {
+
+    if (event.buttons & 1) onClick(event);
+
+  }
 
   let label = el('label', null, name);
 
   let background = el('div', { style: 'position: absolute; top: 0; left: 0; bottom: 0; right: 0; background: black;' });
-  let foreground = el('div', { style: 'position: absolute; top: 0; left: 0; bottom: 0; right: 50%; background: cyan;' });
+  let foreground = el('div', { style: `position: absolute; top: 0; left: 0; bottom: 0; right: ${ 100 - (100 * ((value / options.max) - options.min)) }%; background: cyan;` });
 
-  let slider = el('div', { style: 'flex: 1; position: relative' }, background, foreground);
+  let slider = el('div', { style: 'flex: 1; position: relative', onClick, onMouseMove }, background, foreground);
 
   return el('div', null, label, slider);
 
