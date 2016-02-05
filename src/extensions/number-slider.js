7c9e98ad0223368b07660071dff04f12558158ea
Chris Parker <parker@byhook.com>
2016-02-04 21:41:03 -0500

    add value label to number-slider

--- src/extensions/number-slider.js
+++ src/extensions/number-slider.js
@@ -27,14 +27,17 @@ function render(control, el) {
 
   }
 
-  let label = el('label', null, name);
+  let controlLabel = el('label', null, name);
+  let valueLabel = el('div', { style: 'position: absolute; left: 0; right: 0; text-align: center;' }, value.toPrecision(2));
+
 
   let background = el('div', { style: 'position: absolute; top: 0; left: 0; bottom: 0; right: 0; background: black;' });
-  let foreground = el('div', { style: `position: absolute; top: 0; left: 0; bottom: 0; right: ${ 100 - (100 * ((value / options.max) - options.min)) }%; background: cyan;` });
+  let foreground = el('div', { style: `position: absolute; top: 0; left: 0; bottom: 0; right: ${ 100 - (100 * ((value / options.max) - options.min)) }%; background: #4C6767;` });
 
-  let slider = el('div', { style: 'flex: 1; position: relative', onClick, onMouseMove }, background, foreground);
 
-  return el('div', null, label, slider);
+  let slider = el('div', { style: 'flex: 1; position: relative', onClick, onMouseMove }, background, foreground, valueLabel);
+
+  return el('div', null, controlLabel, slider);
 
 }
 
