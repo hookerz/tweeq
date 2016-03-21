6742b22a5c9e7e57ad846dfd5b5ce50dc9f488b3
Chris Parker <parker@byhook.com>
2016-03-21 16:16:32 -0400

    remove commonjs support (open to reevaluation)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import insertCSS from 'insert-css';
-
+import { element }         from 'deku';
+import insertCSS           from 'insert-css';
 import control, { extend } from './control';
-import container from './container';
-import extensions from './extensions';
-import styles from './styles';
+import container           from './container';
+import extensions          from './extensions';
+import styles              from './styles';
 
 // Inject the styles into the page.
 insertCSS(styles, { prepend: true });
@@ -16,11 +16,4 @@ for (let key in extensions) {
 
 }
 
-// Babel compiles `export default` to `exports.default` which means that any
-// CommonJS module would have to do
-//
-//   const tweeq = require('tweeq').default;
-//
-// which isn't awesome. Using `module.exports` keeps compatability.
-
-module.exports = { container, control, extend };
+export default { container, control, extend, element };
